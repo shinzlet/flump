0b956049c75f1951437bf51633ba70a05b5b184d
Seth Hinz <sethhinz@me.com>
2024-08-08 11:55:10 -0700

    Implemented github actions build / version bumping

---
+++ src/flump/build.py
@@ -0,0 +1,8 @@
+import PyInstaller.__main__
+from pathlib import Path
+
+HERE = Path(__file__).parent.absolute()
+path_to_main = str(HERE / "flump.py")
+
+def install():
+	PyInstaller.__main__.run([path_to_main, '--onefile', '--windowed', '--distpath', 'flump'])
