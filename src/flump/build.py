ce38b107b912680d6fcd3bde305dd24eb812b554
Seth Hinz <sethhinz@me.com>
2024-08-14 13:52:03 -0700

    Automatic windows builds

--- src/flump/build.py
+++ src/flump/build.py
@@ -4,5 +4,8 @@ from pathlib import Path
 HERE = Path(__file__).parent.absolute()
 path_to_main = str(HERE / "../../pyinstaller_entry_point.py")
 
-def install():
+def main():
 	PyInstaller.__main__.run([path_to_main, '--onefile', '--windowed', '--distpath', 'flump', '--name', 'flump'])
+
+if __name__ == '__main__':
+	main()
